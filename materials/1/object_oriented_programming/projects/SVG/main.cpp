95227bf45088852f1a8287dd840795485be2baa1
Alexander Stoichkov <sashostoichkov@gmail.com>
2022-06-19 20:01:48 +0300

    cleaning up the main.cpp

--- materials/1/object_oriented_programming/projects/SVG/main.cpp
+++ materials/1/object_oriented_programming/projects/SVG/main.cpp
@@ -2,25 +2,9 @@
 using namespace std;
 
 #include "./Interface/Interface.h"
-// #include "./File/File.h"
 
 int main() {
   Interface().run();
-
-  // File file("SVGs/figures.svg");
-  // file.load();
-  // // file.print();
-
-  // file.getShapes()[0]->translate(10, 10);
-
-  // file.save("SVGs/figures_new_one.svg");
-
-  // file.getShapes()[0]->translate(-10, -10);
-  // for (int i = 0; i < file.getShapes().size(); i++) {
-  //   file.getShapes()[i]->translate(10, 10);
-  // }
-
-  // file.save("SVGs/figures_new.svg");
 
   return 0;
 }
